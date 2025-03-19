7f7ecd8555e00102c9955d27303cc2e36182a7d3
Ariel <Ariel@DESKTOP-JK4J511>
2025-03-19 07:57:46 -0600

    Agregar archivos de proyecto.

---
+++ CarlosAriel19032025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarlosAriel19032025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
